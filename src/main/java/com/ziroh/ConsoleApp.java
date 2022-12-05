20e160c0730ab4d53265d6ecf22b108c16ec2bd3
Abhigyan Gogoi <abhigyank.gogoi@ziroh.com>
2022-12-05 13:13:58 +0530

    Implemented Min and Max heap and sort for both;

--- src/main/java/com/ziroh/ConsoleApp.java
+++ src/main/java/com/ziroh/ConsoleApp.java
@@ -1,7 +1,17 @@
 package com.ziroh;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+
 public class ConsoleApp {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        LinkedList<Integer> array = new LinkedList<>(Arrays.asList(4, 1, 3, 2, 16, 9, 10, 14, 8, 7));
+        HeapTree heapTree = new HeapTree(array);
+        heapTree.buildMaxHeapTree();
+        heapTree.getHeapTree();
+        heapTree.sortDescending();
+        heapTree.buildMinHeapTree();
+        heapTree.getHeapTree();
+        heapTree.sortAscending();
     }
 }
